e8d47db6a5d41cbec066bbf100ac2344a263a46d
TimBlokdijk <1026135@mborijnland.nl>
2024-01-08 13:32:24 +0100

    Database name changed

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $servername =   "localhost";
 $username   =   "root";
 $password   =   "";
-$dbname     =   "vliegmaatschappij";
+$dbname     =   "Kamadoing";
 
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
