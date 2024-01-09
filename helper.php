35b41b8d1e0d6006b93ed48ef9176b244d8ea00d
TimBlokdijk <1026135@mborijnland.nl>
2024-01-09 11:43:49 +0100

    Helper code
    This is not the final code, it needs to be replaced by the working code.

--- helper.php
+++ helper.php
@@ -1,1 +1,55 @@
+<?php
+require "dbconnect.php";
 
+
+function createVlucht($vliegtuigcode, $tijd, $startbestemming, $eindbestemming) {
+    global $conn;
+
+    try {
+        $stmt = $conn->prepare("INSERT INTO vluchten (vliegtuigcode, tijd, startbestemming, eindbestemming) VALUES (:vliegtuigcode, :tijd, :startbestemming, :eindbestemming)");
+
+        $stmt->bindParam(':vliegtuigcode',      $vliegtuigcode);
+        $stmt->bindParam(':tijd',               $tijd);
+        $stmt->bindParam(':startbestemming',    $startbestemming);
+        $stmt->bindParam(':eindbestemming',     $eindbestemming);
+        
+        $stmt->execute();
+        return true;
+
+    }
+    catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    return false;
+    }  
+}
+
+function readAllVluchten() {
+    global $conn;
+
+    try{
+        $stmt = $conn->prepare("Select * FROM vluchten");
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    return false;
+    }
+}
+
+function readSingleVlucht($vluchtid) {
+    global $conn;
+
+    try {
+        $stmt = $conn->prepare("SELECT * FROM vluchten WHERE id = :id");
+        $stmt->bindParam(':id', $vluchtid);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        return false;
+    }
+}
+?>
