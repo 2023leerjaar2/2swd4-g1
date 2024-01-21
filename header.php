a3f1f00b992415546eb25390166471f657c4b1d4
TimBlokdijk <1026135@mborijnland.nl>
2024-01-21 17:06:41 +0100

    Header

--- header.php
+++ header.php
@@ -1,1 +1,23 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section>
+            <a href="index.php"><img src="images/logo.png" alt="Kamadiong logo" height="70vh"></a>
+        </section>
+        <section>
+            <a href="#">Contact</a>
+            <a href="#">Recepten</a>
+        </section>
+        <section>
+            <a href="#"><img src="images/login_logo.png" alt="Login knop" height="70vh"></a>
+        </section>
+    </header>
+</body>
+</html>
