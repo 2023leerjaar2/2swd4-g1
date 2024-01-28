41dc404a98ddeb8eed7cf9e205e834133eafc5ae
Fprottoy <6019259@mborijnland.nl>
2024-01-28 14:02:31 +0100

    contact pagina
    contact pagina gemaakt met html en scss

--- contact.php
+++ contact.php
@@ -1,1 +1,36 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section>
+            <a href="index.php"><img src="images/logo.png" alt="Kamadiong logo" height="70vh"></a>
+        </section>
+        <section>
+            <a href="#">Contact</a>
+            <a href="#">Recepten</a>
+        </section>
+        <section>
+            <a href="#"><img src="images/login_logo.png" alt="Login knop" height="70vh"></a>
+        </section>
+    </header>
+    <main>
+        <div class="contact-info">
+            <h1>Contact Us</h1>
+            <p>For any inquiries or assistance, please feel free to reach out to us:</p>
+            <ul>
+                <li>Email: info@example.com</li>
+                <li>Phone: +1234567890</li>
+                <li>Address: 123 Street, City, Country</li>
+            </ul>
+        </div>
+        
+    </main>
+</body>
+</html>
