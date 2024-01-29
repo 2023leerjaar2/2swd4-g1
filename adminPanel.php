0196eb64c1c69894ad56ec699d6ab9ff9c53d4c6
Fprottoy <6019259@mborijnland.nl>
2024-01-29 13:23:16 +0100

    admin panel af
    admin panel af met html en css

--- adminPanel.php
+++ adminPanel.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Admin Panel</title>
+</head>
+<body>
+    <header>
+        <section>
+            <a href="index.php"><img src="images/logo.png" alt="Kamadiong logo" height="70vh"></a>
+        </section>
+        <section>
+            <a href="#">Contact</a>
+            <a href="#">Recepten</a>
+        </section>
+        <section>
+            <a href="#"><img src="images/login_logo.png" alt="Login knop" height="70vh"></a>
+        </section>
+    </header>
+    <div class="admin-buttons">
+        <button>New recept</button>
+        <button>Accounts</button>
+        <button>All recepten</button>
+    </div>
+</body>
+</html>
 
