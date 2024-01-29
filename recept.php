08677b67e7af1ff70ad369db6aadc9984e14b8d4
Fprottoy <6019259@mborijnland.nl>
2024-01-29 13:46:37 +0100

    recept pagina is af met html en scss

--- recept.php
+++ recept.php
@@ -1,1 +1,33 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Recipe Page</title>
+</head>
+<body>
+    <div class="recipe">
+        <div class="box ingredients">
+            <h2 class="title">Ingredients</h2>
+            <ul>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Consectetur adipiscing elit</li>
+                <li>Sed do eiusmod tempor</li>
+                <li>Incididunt ut labore et dolore</li>
+                <li>Magna aliqua. Ut enim ad minim veniam</li>
+            </ul>
+        </div>
+        <div class="box image">
+            <img src="images/logo.png" alt="Recipe Image">
+            <div class="caption">Image Caption</div>
+        </div>
+        <div class="box recipe-desc">
+            <h2 class="title">Recipe</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </div>
+</body>
+</html>
